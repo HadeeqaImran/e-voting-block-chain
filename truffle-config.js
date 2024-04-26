6a58833ac8073ec1ef74b025acf121ed5525f81f
Hadeeqa Imran <hadeeqaimran786@gmail.com>
2024-04-26 16:22:14 +0500

    updated truffle-config.js for successful migration of contract

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,11 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const mnemonic = "portion ribbon timber copy slogan orchard magnet picnic result warm pledge defy";
-
 module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      provider: () => new HDWalletProvider(mnemonic, "http://127.0.0.1:7545"),
-      network_id: "*"
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
     },
     develop: {
       port: 8545
